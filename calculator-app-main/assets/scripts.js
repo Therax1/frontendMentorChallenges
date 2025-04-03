3b21b3a8daf4fb873936828685634490fbe77298
Therax1 <nabilmoutouama@gmail.com>
2025-04-03 14:04:03 +0100

    Creation des thèmes. Terminé!

--- calculator-app-main/assets/scripts.js
+++ calculator-app-main/assets/scripts.js
@@ -1,9 +1,32 @@
 const screen  = document.querySelector(".screen-display");
+const Screen = document.querySelector(".screen")
 const buttons  = document.querySelectorAll(".btn");
 const reset  = document.querySelector(".reset");
 const equal  = document.querySelector(".equal");
 const deleted = document.querySelector(".del");
+const themeChange = document.querySelector(".theme-changing")
+const body = document.querySelector("body")
+const green = document.querySelector(".green")
+const keypad = document.querySelector(".numberpOp")
+const up = document.querySelector(".up")
 
+
+themeChange.addEventListener("click", ()=>{
+        Screen.classList.toggle("screen-theme2")
+        screen.classList.toggle("screen-display-theme2")
+        body.classList.toggle("body-theme2")
+        equal.classList.toggle("equal-theme2")
+        green.classList.toggle("green-theme2")
+        buttons.forEach(button => {
+            button.classList.toggle("button-theme2")
+        })
+        keypad.classList.toggle("numberpOp-theme2")
+        up.classList.toggle("up.theme2")
+
+})
+
+
+//FOnctionnalités de la calculatrice
 buttons.forEach(button => {
     button.addEventListener("click", ()=>{
         const value = button.textContent;
