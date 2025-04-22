587b10305a6d5c3c0172df7763013031b551284a
Thérance MOUTOUAMA <nabilmoutouama@gmail.com>
2025-04-22 13:10:51 +0100

    Js now

--- mortgage-repayment-calculator-main/script.js
+++ mortgage-repayment-calculator-main/script.js
@@ -1,8 +1,25 @@
 const buttonclick = document.querySelector(".calculate");
 const part2 = document.querySelector(".part2")
 partCalc = document.querySelector(".part2-calculated")
+const clearAll = document.querySelector(".clearAll")
 
-buttonclick.addEventListener('click', ()=>{
-  part2.classList.toggle("none")
-  partCalc.classList.toggle("none")
-})
+document.querySelectorAll(".type").forEach(input => {
+  input.addEventListener('change', function() {
+    document.querySelectorAll('.option').forEach(div => {
+      div.classList.remove('select-radio')
+    })
+
+    if(this.checked){
+      this.parentElement.classList.add('select-radio')
+    }
+  })
+});
+
+document.querySelector('.clearAll').addEventListener("click", ()=>{
+  
+}) 
+
+// buttonclick.addEventListener('click', ()=>{
+//   part2.classList.toggle("none")
+//   partCalc.classList.toggle("none")
+// })
